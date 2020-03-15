2b3246c3d2bd7fa3c8cc7b299773a7c8016cc3a8
ThePurityOfChaos <derickdew9001@gmail.com>
2020-03-14 19:36:34 -0700

    Problem 2 (excepting the time comparison) COMPLETE!

--- problem_2/src/MaxHeap.java
+++ problem_2/src/MaxHeap.java
@@ -12,45 +12,56 @@ public class MaxHeap implements Heap {
     //all me, no assistance
     public void MaxHeapLogN(Integer[] data) {
         for (int i = 0; i < data.length; i++) //n
-            this.add(data[i]); //logn
+            this.add(data[i]);//logn
     }
 
     // build a heap based on data
     // to be implemented in O(n)
     //O(n) is harder, I needed some help
     public void MaxHeapN(Integer[] data) {
-
+    this.data= new Integer[data.length];
+    this.data= data;
+    this.size=data.length;
+    int lastParent = (size-2)/2;
+    for(int i=lastParent; i>=0; i--)
+    heapifyAll(i);
     }
 
     // add an item to the heap, this is log(n) time
     //had some help from GFG, but changed it up a bit
     public boolean add(Integer item) {
-        if (size >= data.length - 1) {
+        if (size >= data.length){
             return false;
         }
-        size++;
+
         data[size] = item;
         heapifyOne(size);
+        size++;
         return true;
     }
 
     private void heapifyOne(int i) {
-        int curr = i+1;
-        while (data[curr] > data[curr / 2]) {
-            swap(curr, curr / 2);
-            curr = curr / 2;
+        int curr = i;
+        if(data[curr]!=null) {
+            while (data[curr] > data[(curr-1)/ 2]) {
+                System.out.println(data[curr] + " " + data[curr / 2]);
+                    swap(curr, (curr - 1) / 2);
+                    curr = (curr - 1) / 2;
+                }
+            }
         }
-    }
+
 
     private void heapifyAll(int i) {
         if (isLeaf(i)) return;
-        if (data[i] < data[i * 2 + 1] || data[i] < data[i * 2 + 2])
-            if (data[i * 2 + 1] > data[i * 2 + 2]) {
-                swap(i, i * 2 + 1);
-                heapifyAll(i * 2 + 1);
+        if(i*2+1>=data.length-1) return;
+        if (data[i] < data[(i * 2)+1] || data[i] < data[(i * 2) + 2])
+            if (data[(i * 2)+1] > data[(i * 2) + 2]) {
+                swap(i, (i * 2)+1);
+                heapifyAll((i * 2)+1);
             } else {
-                swap(i, i * 2 + 2);
-                heapifyAll(i * 2 + 2);
+                swap(i, (i * 2)+2);
+                heapifyAll((i * 2)+2);
             }
     }
 
@@ -61,12 +72,12 @@ public class MaxHeap implements Heap {
     }
 
     private boolean isLeaf(int i) {
-        return i >= size / 2 && i <= size;
+        return i >= (size / 2)+1&&i<=size;
     }
 
     // return the max item in the heap
     public Integer get() {
-        if (data != null) {
+        if (size != 0) {
             return data[0];
         }
         return null;
@@ -74,9 +85,10 @@ public class MaxHeap implements Heap {
 
     // remove the root item
     public Integer pop() {
-        if (data != null) {
+        if (size!=0) {
             Integer result = data[0];
             data[0] = null;
+            size--;
             if (data.length >= 2)
                 if (data[1] > data[2])
                     heapifyAll(1);
--- problem_2/src/MaxHeapTest.java
+++ problem_2/src/MaxHeapTest.java
@@ -7,15 +7,18 @@ public class MaxHeapTest {
     Integer[][] datasets = {{},{0},{0,1,2},{0,1,2,3,4,5,7,23,55,96,1000,-12},{5,5,5,5,16,4}};
     @Test
     public void MaxHeapLogN(){ //this also tests add due to how I set up MaxHeapLogN();
-        Integer[][] expected = {{},{0},{2,1,0},{1000,96,55,23,7,5,4,3,2,1,0,-12},{16,5,5,5,5,4}};
+        Integer[][] expected = {{},{0},{2,0,1},{1000,96,5,7,55,1,4,0,3,2,23,-12},{16,5,5,5,5,4}};
         MaxHeap[] actual = new MaxHeap[datasets.length];
 
         for(int i=0; i<datasets.length; i++) {
             actual[i] = new MaxHeap(datasets[i].length);
             actual[i].MaxHeapLogN(datasets[i]);
+
         }
-        for(int i=0; i<datasets.length; i++)
-        assertEquals(expected[i],actual[i].toArray());
+        for(int i=0; i<datasets.length; i++) {
+            System.out.println(i);
+            assertEquals(expected[i], actual[i].toArray());
+        }
     }
 
     @Test
@@ -23,6 +26,7 @@ public class MaxHeapTest {
         Integer[] expected = {null,0,2,1000,16};
         MaxHeap[] actual = new MaxHeap[datasets.length];
         for(int i=0; i<datasets.length; i++){
+            actual[i] = new MaxHeap(datasets[i].length);
             actual[i].MaxHeapLogN(datasets[i]);
             assertEquals(expected[i],actual[i].get());
         }
@@ -32,21 +36,25 @@ public class MaxHeapTest {
         Integer[] expected = {null,0,2,1000,16};
         MaxHeap[] actual = new MaxHeap[datasets.length];
         for(int i=0; i<datasets.length; i++){
+            actual[i] = new MaxHeap(datasets[i].length);
             actual[i].MaxHeapLogN(datasets[i]);
             assertEquals(expected[i],actual[i].pop());
+            if(actual[i].size!=0)
             assertTrue(actual[i].size<datasets[i].length);
         }
     }
     @Test
             public void MaxHeapN(){
-    Integer[][] expected = {{},{0},{2,1,0},{1000,96,55,23,7,5,4,3,2,1,0,-12},{16,5,5,5,5,4}};
+    Integer[][] expected = {{},{0},{2,1,0},{1000,96,7,55,4,5,2,23,3,1,0,-12},{16,5,5,5,5,4}};
     MaxHeap[] actual = new MaxHeap[datasets.length];
-
         for(int i=0; i<datasets.length; i++) {
         actual[i] = new MaxHeap(datasets[i].length);
+        System.out.println(i);
         actual[i].MaxHeapN(datasets[i]);
     }
-        for(int i=0; i<datasets.length; i++)
-    assertEquals(expected[i],actual[i].toArray());
+        for(int i=0; i<datasets.length; i++) {
+            System.out.println(i);
+            assertEquals(expected[i], actual[i].toArray());
+        }
     }
 }
